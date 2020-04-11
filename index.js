ce25a51f6437bbd8311f75962e644b977a7e36f9
surmistry <mistrs8@gmail.com>
2020-04-11 19:59:02 -0400

    Initialize helix-generator repository.

---
+++ index.js
@@ -0,0 +1,60 @@
+const commandLineArgs = require('command-line-args');
+
+const optionDefinitions = [
+  {
+    name: 'emojis',
+    alias: 'e',
+    defaultOption: true,
+    multiple: true,
+    type: String,
+  },
+  {
+    name: 'cycles',
+    alias: 'c',
+    type: Number,
+  },
+  {
+    name: 'output',
+    alias: 'o',
+    type: String
+  }
+]
+
+const options = commandLineArgs(optionDefinitions);
+
+const TEMPLATE_HELIX =
+  `01          02
+ 01        02
+  01      02
+    01  02
+      01
+    02  01
+  02      01
+ 02        01
+02          01
+02          01
+ 02        01
+  02      01
+    02  01
+      02
+    01  02
+  01      02
+ 01        02
+01          02
+`;
+
+const { emojis, cycles } = options
+
+const output = emojis.reduce(
+  (updatingHelix, helixValue, index) => {
+    const target = index + 1;
+    let newHelix = updatingHelix.replace(new RegExp(`0${target}`, 'g'), helixValue)
+    return newHelix;
+  },
+  TEMPLATE_HELIX
+);
+
+let outputHelix = '';
+for (let i = 0; i < cycles; i++)
+  outputHelix += output;
+console.log(outputHelix);
---
+++ readme.md
@@ -0,0 +1,15 @@
+# helix generator (WIP)
+
+generate a string of any two characters in combination.
+
+## usage
+
+```
+node index.js <helix array> <period cycle number>
+```
+
+## incoming features
+
+1. implement as npm package
+2. dynamically changing template
+3. extendable number of emojis 
